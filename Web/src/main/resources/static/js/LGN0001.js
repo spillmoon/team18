d283c22269f4087a5e478b53b6b817249036618a
spillmoon <spillmoon@naver.com>
2017-08-02 10:01:39 +0900

    edit

--- Web/src/main/resources/static/js/LGN0001.js
+++ Web/src/main/resources/static/js/LGN0001.js
@@ -1,19 +1,21 @@
-function login() {
-	var userId = $("#user_id").val().trim();
-	var userPwd = $("#user_pw").val().trim();
+$(document).ready(function(){
+	$(".bt_login").click(function(){
+		var userId = $("#user_id").val().trim();
+		var userPwd = $("#user_pw").val().trim();
 
-	$.ajax({
-		type : "POST",
-		url : "/userLogin",
-		data : {
-			"user_id" : userId,
-			"user_pw" : userPwd
-		},
-		success : function(msg) {
-			window.location.href = "main";
-		},
-		error : function(xhr, status, error) {
-			alert("아이디 혹은 비밀번호가 잘못되었습니다.");
-		}
+		$.ajax({
+			type : "POST",
+			url : "/userLogin",
+			data : {
+				"user_id" : userId,
+				"user_pw" : userPwd
+			},
+			success : function(msg) {
+				window.location.href = "main";
+			},
+			error : function(xhr, status, error) {
+				alert("아이디 혹은 비밀번호가 잘못되었습니다.");
+			}
+		});
 	});
-}
+});
